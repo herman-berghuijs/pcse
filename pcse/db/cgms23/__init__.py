7603d8d0adce7296ef17f2b3fee406808457874f
Wit, Allard de <allard.dewit@wur.nl>
2024-01-25 12:54:34 +0100

    CGMS23EUR drivers for potential production

---
+++ pcse/db/cgms23/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2004-2023 Wageningen Environmental Research
+# Steven Hoek (steven.hoek@wur.nl), Allard de Wit November 2023
+"""Tools for reading weather data and timer, soil and site parameters
+from a CGMS23 compatible database.
+
+"""
+
+from .data_providers import *
